46684c731612c93b0c285dc9734aae5eeb1782c4
jean-marie leroux <jmleroux.pro@gmail.com>
2020-11-03 16:40:40 +0100

    Fix QR code text (#15)

--- src/Attestation/Justifications.php
+++ src/Attestation/Justifications.php
@@ -7,19 +7,19 @@ namespace Jmleroux\CovidAttestation\Attestation;
 class Justifications
 {
     private array $justifications = [
-        'pro' => [
+        'travail' => [
             'label' => "Déplacement professionnel",
             'description' => "Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle ou un établissement d’enseignement ou de formation, déplacements professionnels ne pouvant être différés, déplacements pour un concours ou un examen.",
         ],
-        'shopping' => [
+        'achats' => [
             'label' => "Achats de première nécessité",
             'description' => "Déplacements pour effectuer des achats de fournitures nécessaires à l'activité professionnelle, des achats de première nécessité³ dans des établissements dont les activités demeurent autorisées, le retrait de commande et les livraisons à domicile.",
         ],
-        'health' => [
+        'sante' => [
             'label' => "Santé",
             'description' => "Consultations, examens et soins ne pouvant être ni assurés à distance ni différés et l’achat de médicaments.",
         ],
-        'family' => [
+        'famille' => [
             'label' => "Motif familial impérieux",
             'description' => "Déplacements pour motif familial impérieux, pour l'assistance aux personnes vulnérables et précaires ou la garde d'enfants.",
         ],
@@ -27,19 +27,19 @@ class Justifications
             'label' => "Déplacement des personnes en situation de handicap",
             'description' => "Déplacement des personnes en situation de handicap et leur accompagnant.",
         ],
-        'leasure' => [
+        'sport_animaux' => [
             'label' => "Déplacement bref",
             'description' => "Déplacements brefs, dans la limite d'une heure quotidienne et dans un rayon maximal d'un kilomètre autour du domicile, liés soit à l'activité physique individuelle des personnes, à l'exclusion de toute pratique sportive collective et de toute proximité avec d'autres personnes, soit à la promenade avec les seules personnes regroupées dans un même domicile, soit aux besoins des animaux de compagnie.",
         ],
-        'justice' => [
-            'label' => "Justice ou service public",
+        'convocation' => [
+            'label' => "Convocation justice ou service public",
             'description' => "Convocation judiciaire ou administrative et pour se rendre dans un service public",
         ],
-        'administrative' => [
+        'missions' => [
             'label' => "Mission administrative",
             'description' => "Participation à des missions d'intérêt général sur demande de l'autorité administrative",
         ],
-        'school' => [
+        'enfants' => [
             'label' => "Déplacement scolaire",
             'description' => "Déplacement pour chercher les enfants à l’école et à l’occasion de leurs activités périscolaires",
         ],

b5b0ed9866e4518c2948437c5d979b7cb7ddb7e8
vibea <vincent.beauvivre@smile.fr>
2020-11-03 19:13:07 +0100

    Allow multi reason + set date to 10 minutes ago

--- src/Attestation/UserForm.php
+++ src/Attestation/UserForm.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Jmleroux\CovidAttestation\Attestation;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
